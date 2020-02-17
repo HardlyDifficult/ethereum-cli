10d2c7becf285ea76210ee70c3a82923bc9b73bf
NickCuso <hardlydiff@gmail.com>
2020-02-17 15:09:59 -0800

    Initial implementation

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-ethereum-cli
+# Airdrop
+
+To run an airdrop, first clone this repo and then run:
+
+```
+yarn
+```
+(or npm i)
+
+Create a file in the directory named `.private_key` and paste in a key.  Do not use you usual wallet for this.  One way to create a private key is to generate a new account with Metamask and then under 'Account Details' 'Export Private Key'.
+
+When you run the script it will tell you exactly how much ETH it needs. You don't need to send more than it asks for.
+
+Now update `settings.json`:
+ - Set the provider to the correct network, e.g. `mainnet`
+ - Update config including the airdrop amount
+ - Populate the array of addresses
+
+Run `yarn start` and the bot will tell you how much it needs. e.g.
+
+```
+Script account: 0x44446D0d742c7BA82238Fe5Ad047daba53F76dd9 has 0 ETH
+Bot cannot afford this drop - we need 0.0020420042 more
+```
+
+Send the script account's address exactly the eth specified (make sure you are on the same network!) and then run the bot again.
---
+++ index.js
@@ -0,0 +1,84 @@
+const Web3 = require('web3');
+const fs = require('fs');
+const BigNumber = require('bignumber.js');
+
+function sleep(ms)
+{
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+(async () =>
+{
+  if(!fs.existsSync(`${process.cwd()}/settings.json`))
+  {
+    console.log('Missing `settings.json`');
+    process.exit();
+  }
+  
+  const settings = require(`${process.cwd()}/settings.json`)
+  
+  if(!fs.existsSync(`${process.cwd()}/settings.json`))
+  {
+    console.log('Missing `.private_key`');
+    process.exit();
+  }
+  let privateKey = fs.readFileSync(`${process.cwd()}/.private_key`).toString()
+  privateKey = `0x${privateKey.replace('0x', '')}`;
+  
+  if(!settings.provider)
+  {
+    console.log("`settings.json` requires a web3 `provider`")
+    process.exit();
+  }
+  const web3 = new Web3(settings.provider);
+  const scriptAccount = web3.eth.accounts.privateKeyToAccount(privateKey)
+  
+  const ethBalance = new BigNumber(await web3.eth.getBalance(scriptAccount.address))
+  console.log(`Script account: ${scriptAccount.address} has ${web3.utils.fromWei(ethBalance.toFixed(), 'ether')} ETH`);
+
+  const dropCount = settings.accounts.length
+  if(!dropCount)
+  {
+    console.log("Missing `settings.accounts`");
+    process.exit();
+  }
+  if(!settings.amountToDropInEther)
+  {
+    console.log("Missing `settings.amountToDropInEther`");
+    process.exit();
+  }
+  if(!settings.gasPriceInGwei)
+  {
+    console.log("Missing `settings.gasPriceInGwei`");
+    process.exit();
+  }
+
+  const dropAmountInWei = web3.utils.toWei(settings.amountToDropInEther, 'ether')
+  const gasPriceInWei = web3.utils.toWei(settings.gasPriceInGwei, 'gwei')
+  const totalCostPerDropInWei = new BigNumber(gasPriceInWei).times(21000).plus(dropAmountInWei)
+  let costInWei = new BigNumber(dropCount).times(totalCostPerDropInWei)
+  if(ethBalance.lt(costInWei))
+  {
+    const deltaInEth = web3.utils.fromWei(costInWei.minus(ethBalance).toFixed(), 'ether')
+    console.log(`Bot cannot afford this drop - we need ${deltaInEth} more`)
+    process.exit();
+  }
+
+  let nonce =  await web3.eth.getTransactionCount(
+    scriptAccount.address,
+    "pending"
+  );
+  
+  for(let i = 0; i < settings.accounts.length; i++)
+  {
+    const account = settings.accounts[i];
+    console.log(`Dropping to ${account}`)
+    const tx = await scriptAccount.signTransaction({ to: account, value:dropAmountInWei, gas: 21000, gasPrice: gasPriceInWei, nonce: nonce++ })
+
+    web3.eth.sendSignedTransaction(tx.rawTransaction)
+    await sleep(1000) // Sleeping so we don't get banned by the provider
+  }
+
+  console.log("Completed successfully");
+  process.exit()
+})()
